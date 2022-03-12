30578036230f8e999b01cf26254aac67377d426f
frunox <jrcannon@msn.com>
2022-03-12 15:49:05 -0600

    add liveview files

---
+++ lib/calendex_web/live/page_live.ex
@@ -0,0 +1,12 @@
+defmodule CalendexWeb.PageLive do
+  use CalendexWeb, :live_view
+
+  # alias CalendlexWeb.Components.EventType
+
+  # @impl LiveView
+  # def mount(_params, _session, socket) do
+  #   event_types = Calendlex.available_event_types()
+
+  #   {:ok, assign(socket, event_types: event_types), temporary_assigns: [event_types: []]}
+  # end
+end
