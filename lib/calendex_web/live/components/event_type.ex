f2be64b28be09b82e1e627b785fd72f298f3b288
frunox <jrcannon@msn.com>
2022-03-13 21:01:46 -0500

    minor style change

--- lib/calendex_web/live/components/event_type.ex
+++ lib/calendex_web/live/components/event_type.ex
@@ -5,7 +5,7 @@ defmodule CalendexWeb.Components.EventType do
     ~H"""
     <%= live_redirect to: @path do %>
       <div class="flex items-center p-6 pb-20 text-gray-400 bg-white border-t border-gray-300 cursor-pointer hover:bg-gray-200 gap-x-4">
-        <div {[class: "inline-block w-8 h-8 #{@event_type.color}-bg rounded-full border-2 border-white"]}></div>
+        <div {[class: "inline-block w-8 h-6 #{@event_type.color}-bg rounded-full border-2 border-black"]}></div>
         <h3 class="font-bold text-gray-900"><%= @event_type.name %></h3>
         <div class="ml-auto text-xl"><i class="fas fa-caret-right"></i></div>
       </div>
