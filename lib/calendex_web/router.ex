99d08dc1abb026aaa8f3c6f3782994f92482af70
frunox <jrcannon@msn.com>
2022-03-20 11:09:35 -0500

    configure BasicAuth w/ plug, private live_session, admin view and template

--- lib/calendex_web/router.ex
+++ lib/calendex_web/router.ex
@@ -1,6 +1,8 @@
 defmodule CalendexWeb.Router do
   use CalendexWeb, :router
 
+  import Plug.BasicAuth
+
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
@@ -14,11 +16,29 @@ defmodule CalendexWeb.Router do
     plug :accepts, ["json"]
   end
 
+  # auth pipeline
+  pipeline :auth do
+    plug :basic_auth, Application.compile_env(:calendex, :basic_auth)
+  end
+
   # scope "/", CalendexWeb do
   #   pipe_through :browser
 
   #   live "/", PageLive
   # end
+
+  # private live_session for auth
+  live_session :private, on_mount: {CalendexWeb.Live.InitAssigns, :private} do
+    scope "/admin", CalendexWeb.Admin do
+      pipe_through :browser
+      pipe_through :auth
+
+      live "/", EventTypesLive
+      # live "/event_types/new", NewEventTypeLive
+      # live "/event_types/:id", EditEventTypeLive
+      # live "/scheduled_events", ScheduledEventsLive
+    end
+  end
 
   # public route for live session
   live_session :public, on_mount: CalendexWeb.Live.InitAssigns do
