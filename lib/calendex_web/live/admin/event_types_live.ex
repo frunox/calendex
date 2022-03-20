97d1b48e68f106a7062fd5a4bfa82be65b2c6772
frunox <jrcannon@msn.com>
2022-03-20 13:14:12 -0500

    create admin layout page

--- lib/calendex_web/live/admin/event_types_live.ex
+++ lib/calendex_web/live/admin/event_types_live.ex
@@ -1,8 +1,17 @@
 defmodule CalendexWeb.Admin.EventTypesLive do
-  use CalendexWeb, :live_view
+  use CalendexWeb, :admin_live_view
 
   def mount(_params, _session, socket) do
     IO.inspect(socket.assigns, label: "             socket.assigns in admin")
     {:ok, socket, temporary_assigns: [event_types: []]}
+  end
+
+  def handle_params(_, _, socket) do
+    socket =
+      socket
+      |> assign(section: "event_types")
+      |> assign(page_title: "Event Types")
+
+    {:noreply, socket}
   end
 end
